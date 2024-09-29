d10cf03b317ec65f51027892ee5bfafe9c48026a
sebasurb01 <sebasurb01@icloud.com>
2024-09-29 16:37:32 -0500

    clase6

--- clase3.py
+++ clase3.py
@@ -5,3 +5,27 @@ print("helloworld")
 print("bye bye ")
 print(saludo)
 print(nom1bre)
+print(type(nom1bre))
+caracter='j'
+name='''juan 
+sanchez
+u'''
+name='  sebas  '
+last_name='san'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name+" "+last_name)
+print(name*5)
+print(len(name))
+print(len(last_name))
+print(len(name+last_name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
